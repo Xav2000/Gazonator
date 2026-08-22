42b76e48152127635a45047f5a4a3c4ce591df80
Xav2000 <97745002+Xav2000@users.noreply.github.com>
2026-08-22 21:27:27 +0200

    Update setup.py with dependencies and correct maintainer info

--- src/mower_hardware/setup.py
+++ src/mower_hardware/setup.py
@@ -4,19 +4,23 @@ package_name = 'mower_hardware'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'rclpy',
+        'pyserial',
+    ],
     zip_safe=True,
-    maintainer='xav2000',
-    maintainer_email='todo@todo.com',
-    description='Mower Hardware Package',
-    license='TODO: License declaration',
+    maintainer='Xavier Rossignol',
+    maintainer_email='xavier.rossignol@free.fr',
+    description='Hardware interface package for the autonomous mower',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
